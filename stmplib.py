fd09fac0cc3e2f03f79e16206dd78f444ecc8f06
hxp-358 <1905318603@qq.com>
2017-07-08 17:48:25 +0800

    Update stmplib.py

--- stmplib.py
+++ stmplib.py
@@ -3,12 +3,16 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 from subprocess import check_output
-
+# 第三方 SMTP 服务
 receiver = '11111111@qq.com'
-mail_host='smtp.qq.com'
+#设置服务器
+mail_host='smtp.qq.com'  
+#用户名
 mail_user='1111111@qq.com'
+#授权码
 mail_pass='授权码'
 sender=mail_user
+#接收邮件邮箱
 receivers=[receiver]
 
 log = 'Hello world!'
